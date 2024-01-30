2696a124ac5c526cf07883c740df7ec280902156
flooijdt <flooijdt@posteo.de>
2024-01-30 16:08:05 -0300

    default commit.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn main() {
+    let mut url = String::new();
+    let mut username = String::new();
+
+    println!("Enter the url for your caldav account: ");
+
+    io::stdin()
+        .read_line(&mut url)
+        .expect("Failed to read input");
+
+    println!("Enter the username for your caldav account: ");
+
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Failed to read input");
+
+    println!("Enter the password for your caldav account: ");
+
+    // TODO have yet to confirm if this way of handling passwords is secure
+    let password: String = rpassword::read_password().unwrap();
+
+    let agent = ureq::Agent::new();
+    let url = url::Url::parse(url.as_str()).expect("could not parse the given url.");
+
+    let credentials = minicaldav::Credentials::Basic(
+        String::from(username.as_str().trim()),
+        String::from(password.as_str().trim()),
+    );
+
+    let calendars = minicaldav::get_calendars(agent.clone(), &credentials, &url).unwrap();
+
+    for calendar in calendars {
+        println!("{:?}", calendar);
+    }
+}
