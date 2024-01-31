b8292fda170d247ab71d3a42cab97bb9775418ed
flooijdt <flooijdt@posteo.de>
2024-01-31 19:23:53 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,24 @@ fn main() {
 
     // TODO have yet to confirm if this way of handling passwords is secure
     let password: String = rpassword::read_password().unwrap();
+    println!("1");
 
     let agent = ureq::Agent::new();
+    println!("2");
     let url = url::Url::parse(url.as_str()).expect("could not parse the given url.");
+    println!("3");
 
     let credentials = minicaldav::Credentials::Basic(
         String::from(username.as_str().trim()),
         String::from(password.as_str().trim()),
     );
+    println!("4");
 
     let calendars = minicaldav::get_calendars(agent.clone(), &credentials, &url).unwrap();
+    println!("5");
 
     for calendar in calendars {
         println!("{:#?}", calendar);
     }
+    println!("6");
 }
